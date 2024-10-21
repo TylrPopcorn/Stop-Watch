dfb8a30ff896ae94f0e5d41cd2dd2fb1ba59ce3d
TylrPopcorn <tyler.mcmillan99@yahoo.com>
2024-10-20 20:29:02 -0400

    Footer

---
+++ src/Components/Footer.svelte
@@ -0,0 +1,19 @@
+<footer>
+    <div class="copyright / has-text-warning">
+        Copyright 2024 Stop Watch
+    </div>
+</footer>
+
+<style>
+    footer {
+        padding: 90px;
+        text-align: center;
+    }
+
+    .copyright {
+        font-size: 14px;
+        display: inline-block;
+        padding: 20px;
+        border-top: 1px solid #f1a210;
+    }
+</style>
